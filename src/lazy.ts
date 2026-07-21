1b0e41577f49b554884f02741f69fcd704c37627
Stephen Muteti <stephenmuteti09@gmail.com>
2026-07-21 20:17:13 +0300

    Initial commit

--- src/lazy.ts
+++ src/lazy.ts
@@ -1,6 +1,6 @@
 import { lazy } from "react";
 
 
-export const NewClientOrders = lazy(() => import("@/pages/client/NewClientOrders"));
+export const NewClientOrders = lazy(() => import("@/pages/NewClientOrders"));
 export const OrderDetails = lazy(() => import("@/pages/OrderDetails"));
 export const ClientLayout = lazy(() => import("@/components/ClientLayout"));
